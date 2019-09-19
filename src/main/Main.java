a1c1b865c7e34e19f27ff4767ff0db07262c1070
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-09-19 20:38:45 +0530

    display the details of books using class:Book

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,30 @@
 package main;
 
+import book.Book;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfEntries = scanner.nextInt();
+
+        if (numberOfEntries == 0) {
+            System.out.println("N/A");
+        } else {
+            Book[] books = new Book[numberOfEntries];
+            scanner.nextLine().trim();
+            for (int i = 0; i < numberOfEntries; i++) {
+                String bookName = scanner.nextLine().trim();
+                String authorName = scanner.nextLine().trim();
+                String isbn = scanner.nextLine().trim();
+                books[i] = new Book(bookName, authorName, isbn);
+            }
+            for (int i = 0; i < books.length; i++) {
+                System.out.println(books[i]);
+
+            }
+        }
 
         scanner.close();
     }
