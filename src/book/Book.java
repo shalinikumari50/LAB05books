f61e3047bb69cc385c862872618a00a69149262f
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-09-19 20:07:17 +0530

    add getter and setter methods for all variables

--- src/book/Book.java
+++ src/book/Book.java
@@ -4,4 +4,29 @@ public class Book {
     private String bookName;
     private String authorName;
     private String isbn;
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
 }
